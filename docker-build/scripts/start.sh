6b92fe9e43b74f6b9aae44a706491cf11614122d
lovehzj <1922919664@qq.com>
2022-01-09 15:39:15 +0800

    add start daemon script

--- docker-build/scripts/start.sh
+++ docker-build/scripts/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-mkdir /opt/sh/cassandra/logs
-
-java -Xmx128M -Xms128M -XX:MaxDirectMemorySize=256M -jar /opt/sh/cassandra/mate/cassandra-mate.jar >/opt/sh/cassandra/logs/cassandra_mate.stdout.log 2>/opt/sh/cassandra/logs/cassandra_mate.stderr.log
+DIR="$( cd "$( dirname "$0"  )" && pwd  )"
+bash -x $DIR/start-daemon.sh
 tail -f /dev/null
