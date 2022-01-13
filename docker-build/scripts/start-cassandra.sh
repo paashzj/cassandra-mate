a81f272c7bebbff5dd1b6372b2af7be456f3bc54
lovehzj <1922919664@qq.com>
2022-01-13 23:17:41 +0800

    Release docker image binary

--- docker-build/scripts/start-cassandra.sh
+++ docker-build/scripts/start-cassandra.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/opt/sh/cassandra/bin/cassandra -R >/opt/sh/cassandra/logs/cassandra.log 2>/opt/sh/cassandra/logs/cassandra_error.log
+mkdir -p $CASSANDRA_HOME/logs
+
+$CASSANDRA_HOME/bin/cassandra -R >>$CASSANDRA_HOME/logs/cassandra.log 2>>$CASSANDRA_HOME/logs/cassandra_error.log
--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir $CASSANDRA_HOME/logs
+mkdir -p $CASSANDRA_HOME/logs
 
 java -Xmx128M -Xms128M -XX:MaxDirectMemorySize=256M -jar $CASSANDRA_HOME/mate/cassandra-mate.jar >>$CASSANDRA_HOME/logs/cassandra_mate.stdout.log 2>>$CASSANDRA_HOME/logs/cassandra_mate.stderr.log
